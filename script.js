6a51beb36ae209631a952a68ff0f074a473797ff
largefort <106451813+largefort@users.noreply.github.com>
2023-06-24 16:51:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-let skillNames = ["Shadow Strike", "Dragon Fury", "Storm Bolt", "Arcane Surge", "Venomous Bite", "Holy Smite", "Inferno Blaze", "Frost Nova", "Earthquake", "Divine Shield", "Thunderstorm", "Rapid Shot", "Crippling Blow", "Soul Drain", "Whirlwind Slash"];
-let skills = [], currency = 0, unlockedSkills = [];
+const skillNames = [
+  "Shadow Strike",
+  "Dragon Fury",
+  "Storm Bolt",
+  "Arcane Surge",
+  "Venomous Bite",
+  "Holy Smite",
+  "Inferno Blaze",
+  "Frost Nova",
+  "Earthquake",
+  "Divine Shield",
+  "Thunderstorm",
+  "Rapid Shot",
+  "Crippling Blow",
+  "Soul Drain",
+  "Whirlwind Slash"
+];
 
-const initSkills = () => {
+let skills = [];
+let currency = 0;
+let unlockedSkills = [];
+
+function initSkills() {
   for (let i = 0; i < skillNames.length; i++) {
-    skills.push({ name: skillNames[i], level: (i === 0) ? 0 : -1, exp: 0 });
+    skills.push({
+      name: skillNames[i],
+      level: (i === 0) ? 0 : -1,
+      exp: 0
+    });
   }
 }
 
-const trainSkill = (skillIndex) => {
-  let skill = skills[skillIndex - 1];
+function trainSkill(skillIndex) {
+  const skill = skills[skillIndex - 1];
   skill.exp += 10;
 
   if (skill.exp >= 100) {
     skill.exp = 0;
     skill.level++;
     currency += 10;
-    document.getElementById('currency').textContent = currency;
+    document.getElementById('currency').textContent = currency.toLocaleString('en-US');
     unlockSkills();
   }
 
   updateSkill(skillIndex);
 }
 
-const unlockSkills = () => {
+function unlockSkills() {
   for (let i = 1; i < skillNames.length; i++) {
     if (skills[0].level >= 100) {
       unlockedSkills.push(i + 1);
@@ -30,19 +53,19 @@ const unlockSkills = () => {
   }
 }
 
-const updateSkill = (skillIndex) => {
-  let skill = skills[skillIndex - 1];
-  let skillLevelElement = document.getElementById('skill' + skillIndex + '-level');
-  let skillExpElement = document.getElementById('skill' + skillIndex + '-exp');
+function updateSkill(skillIndex) {
+  const skill = skills[skillIndex - 1];
+  const skillLevelElement = document.getElementById(`skill${skillIndex}-level`);
+  const skillExpElement = document.getElementById(`skill${skillIndex}-exp`);
 
-  skillLevelElement.textContent = skill.level === 0 ? "N/A" : skill.level;
-  skillExpElement.textContent = skill.exp;
+  skillLevelElement.textContent = skill.level === 0 ? "N/A" : skill.level.toLocaleString('en-US');
+  skillExpElement.textContent = skill.exp.toLocaleString('en-US');
 }
 
-const buyAutoTrain = () => {
+function buyAutoTrain() {
   if (currency >= 100) {
     currency -= 100;
-    document.getElementById('currency').textContent = currency;
+    document.getElementById('currency').textContent = currency.toLocaleString('en-US');
     enableAutoTrain();
     alert('Auto-Train feature purchased successfully!');
   } else {
@@ -52,33 +75,36 @@ const buyAutoTrain = () => {
 
 let trainingInterval;
 
-const enableAutoTrain = () => {
-  trainingInterval = setInterval(() => {
+function enableAutoTrain() {
+  trainingInterval = setInterval(function() {
     trainSkill(1);
     trainSkill(2);
   }, 100);
 }
 
-const disableAutoTrain = () => {
+function disableAutoTrain() {
   clearInterval(trainingInterval);
 }
 
-const saveGame = () => {
-  let saveData = { skills: skills, currency: currency };
+function saveGame() {
+  const saveData = {
+    skills: skills,
+    currency: currency
+  };
 
   localStorage.setItem('skillQuestSaveData', JSON.stringify(saveData));
   alert('Game saved successfully!');
 }
 
-const loadGame = () => {
-  let savedData = localStorage.getItem('skillQuestSaveData');
+function loadGame() {
+  const savedData = localStorage.getItem('skillQuestSaveData');
 
   if (savedData) {
-    let saveData = JSON.parse(savedData);
+    const saveData = JSON.parse(savedData);
     skills = saveData.skills;
     currency = saveData.currency;
     updateAllSkills();
-    document.getElementById('currency').textContent = currency;
+    document.getElementById('currency').textContent = currency.toLocaleString('en-US');
     unlockSkills();
     alert('Game loaded successfully!');
   } else {
@@ -86,30 +112,30 @@ const loadGame = () => {
   }
 }
 
-const updateAllSkills = () => {
+function updateAllSkills() {
   for (let i = 1; i <= skills.length; i++) {
     updateSkill(i);
   }
 }
 
-const initGame = () => {
+function initGame() {
   initSkills();
   updateAllSkills();
-  document.getElementById('currency').textContent = currency;
+  document.getElementById('currency').textContent = currency.toLocaleString('en-US');
   unlockSkills();
 }
 
-const enterGame = () => {
+function enterGame() {
   document.getElementById('changelog-container').style.display = 'none';
   document.getElementById('game-container').style.display = 'block';
   initGame();
 }
 
-window.onload = () => {
-  let savedData = localStorage.getItem('skillQuestSaveData');
+window.onload = function() {
+  const savedData = localStorage.getItem('skillQuestSaveData');
 
   if (savedData) {
-    let confirmLoad = confirm('Saved game data found! Do you want to load the saved game?');
+    const confirmLoad = confirm('Saved game data found! Do you want to load the saved game?');
     if (confirmLoad) {
       loadGame();
       enterGame();
